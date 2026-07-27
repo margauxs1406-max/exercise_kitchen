ea39e54fa9788177b729382dab42e6658ec31813
Margaux Silva <m.silva@ifingo.com>
2026-07-28 08:09:27 +1100

    Nettoyage code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.exercise_kitchen"
+    namespace = "com.margauxsilva.exercise_kitchen"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.exercise_kitchen"
+        applicationId = "com.margauxsilva.exercise_kitchen"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
